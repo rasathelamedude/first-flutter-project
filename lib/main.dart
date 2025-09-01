f3b4fa616dd8c80121537ab667b2ee8e065e3810
rasathelamedude <rasyar.safen19@gmail.com>
2025-09-01 11:49:11 +0300

    Worked with Widgets like Scaffold and AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("Hello!"));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("My First Flutter App!")),
+        body: Text("This is my default"),
+      ),
+    );
   }
 }
