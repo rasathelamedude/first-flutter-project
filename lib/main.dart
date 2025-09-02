f383fdad60443b94ba736b4e4601300943495d1d
rasathelamedude <rasyar.safen19@gmail.com>
2025-09-02 11:35:29 +0300

    Statefull & Stateless Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,56 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+// Refactored from a stateless widget to a statefull widget;
+// We need two classes for statefull widgets, one that can be re-built (Widget), another that is persistant (WidgetState);
+// Generally a state is data or information that your widget uses;
+// Statefull => Internal state can be altered;
+// Stateless => Internal state can't be changed;
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var currentQuestion = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestion += 1;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("My First Flutter App!")),
-        body: Text("This is my default"),
+        body: Column(
+          children: [
+            Text(questions[currentQuestion]),
+            ElevatedButton(onPressed: answerQuestion, child: Text("Black")),
+            ElevatedButton(
+              onPressed: () => print("Answer 2 chosen"),
+              child: Text("Green"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print("Answer 3 chosen!");
+              },
+              child: Text("Blue"),
+            ),
+          ],
+        ),
       ),
     );
   }
