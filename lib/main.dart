cda2ac1aec890b04d961967e31b2e85eac6b1895
rasathelamedude <rasyar.safen19@gmail.com>
2025-09-06 12:15:04 +0300

    Conditional Widget Rendering

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,18 @@ class MyApp extends StatefulWidget {
 // Main Application's State;
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  static const List<QuestionModel> _questions = [
+    QuestionModel(
+      questionText: "What is your favorite color?",
+      answers: ["White", "Red", "Blue"],
+    ),
+    QuestionModel(
+      questionText: "What is your favorite animal?",
+      answers: ["Cat", "Dog", "Bird"],
+    ),
+  ]; // Use a class to instantiate questions and answers; Map can be used as well;
 
+  // Button answer handler;
   void _questionAnswer() {
     setState(() {
       _questionIndex += 1;
@@ -29,29 +40,19 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    // Use a class to instantiate questions and answers; Map can be used as well;
-    const List<QuestionModel> _questions = [
-      QuestionModel(
-        questionText: "What is your favorite color?",
-        answers: ["White", "Red", "Blue"],
-      ),
-      QuestionModel(
-        questionText: "What is your favorite animal?",
-        answers: ["Cat", "Dog", "Bird"],
-      ),
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("My First Flutter App!")),
-        body: Column(
-          children: [
-            Question(_questions[_questionIndex].questionText), // Question
-            ..._questions[_questionIndex].answers!.map(
-              (answer) => AnswerButton(answer, _questionAnswer),
-            ), // Answer buttons
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Column(
+                children: [
+                  Question(_questions[_questionIndex].questionText), // Question
+                  ..._questions[_questionIndex].answers!.map(
+                    (answer) => AnswerButton(answer, _questionAnswer),
+                  ), // Answer buttons
+                ],
+              )
+            : Text("You did it!"),
       ),
     );
   }
